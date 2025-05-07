efee85eaf4b203a8f87e259a88503958ae3c95f0
Areebabhutto <bscs2312315@szabist.pk>
2025-05-07 16:28:03 +0000

    GET Method

--- HRAPI/index.js
+++ HRAPI/index.js
@@ -17,7 +17,7 @@ app.get('/',async(req,res)=>{
 
 app.get('/country',async(req,res)=>{
     try{
-        const result = await pool.query('select * from countries');
+        const result = await pool.query('select e.employee_id, m.first_name as manager_firstName, m.last_name as manager_lastName, d.department_name,l.location_id, l.city from employees e left outer join employees m on e.manager_id = m.employee_id left outer join departments d on e.department_id = d.department_id left outer join locations l on d.location_id = l.location_id');
         res.json(result.rows);
     }catch(err)
     {
